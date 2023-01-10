eb661e5dc986dff3f06ff341539bf935cc03aba8
Ruan da Hora <ruanpablosnake@gmail.com>
2023-01-10 19:27:50 -0300

    Initial Commit

---
+++ Navigation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Marcelo Hora on 10/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Marcelo Hora on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
