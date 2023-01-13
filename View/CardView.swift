4dbeee3bdb6d7d0ebeedee30fea2be6d2e1d3ef7
Ruan da Hora <ruanpablosnake@gmail.com>
2023-01-13 09:58:42 -0300

    App Update

--- View/CardView.swift
+++ View/CardView.swift
@@ -5,15 +5,25 @@ struct CardView: View {
     var body: some View {
         VStack {
             VStack (alignment: .leading, spacing: 16.0){
+                
+                VStack {
+                    Image("BackGround")
+                        .resizable()
+                        .frame(width: 45, height: 45, alignment: .trailing)
+                    .cornerRadius(12)
+                    
+                   
+                  
+                }
                 Text("Best Musics")
                     .font(.title.bold())
                 Text("These are the best songs of my year 2022, in an app for me to learn to program iOS.")
-                    .opacity(0.6)
+                    .opacity(0.7)
                 Text("From day 01 to the last.")
                     .opacity(0.6)
             }
                 .padding(.all)
-                .frame(width: 300, height: 200)
+                .frame(width: 300, height: 300)
                 .foregroundColor(.white)
                 .background(Image("BackGround"))
                 .cornerRadius(30)
