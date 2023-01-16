56c9d930e7ff5ec066946277f907ee137ce928ea
Ruan da Hora <ruanpablosnake@gmail.com>
2023-01-16 00:11:55 -0300

    Menssage Private

---
+++ View/MsPrivado1View.swift
@@ -0,0 +1,20 @@
+//
+//  MsPrivado1View.swift
+//  Navigation
+//
+//  Created by Marcelo Hora on 16/01/23.
+//
+
+import SwiftUI
+
+struct MsPrivado1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MsPrivado1View_Previews: PreviewProvider {
+    static var previews: some View {
+        MsPrivado1View()
+    }
+}
