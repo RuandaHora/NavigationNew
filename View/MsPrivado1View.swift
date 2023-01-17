9cdceb36b452f4a525da3bc40ffa64d001c6b889
Ruan da Hora <ruanpablosnake@gmail.com>
2023-01-17 00:44:01 -0300

    MsPrivate Update

--- View/MsPrivado1View.swift
+++ View/MsPrivado1View.swift
@@ -8,39 +8,9 @@
 import SwiftUI
 
 struct MsPrivado1View: View {
-    @State var show = false
-    @State var search = ""
     var body: some View {
         HStack {
-            ZStack{
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 0, height: 0)
-                    .background(Image("Verde"))
-                VStack{
-                    HStack{
-                        Spacer()
-                        NavigationLink (destination: AccontView()) {
-                            Image("Image 2")
-                                .resizable()
-                                .background(Image("Laranja"))
-                                .frame(width: 50, height: 50)
-                                .cornerRadius(100)
-                                .padding(8)
-                            
-                            Spacer()
-                        }
-                    }
-                    Spacer()
-                    HStack{
-                        
-                            TextField("  Digite", text: self.$search)
-                                .frame(width: 250, height: 40)
-                                .background(.ultraThinMaterial)
-                                .cornerRadius(20)
-                        
-                    }
-                }
-            }
+            MsPrivado1()
         }
     }
 }
@@ -48,5 +18,45 @@ struct MsPrivado1View: View {
 struct MsPrivado1View_Previews: PreviewProvider {
     static var previews: some View {
         MsPrivado1View()
+    }
+}
+
+struct MsPrivado1: View {
+    @State var show = false
+    @State var search = ""
+    var body: some View {
+        ZStack{
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 0, height: 0)
+                .background(Image("Verde"))
+            VStack{
+                HStack{
+                    Spacer()
+                    NavigationLink (destination: AccontView()) {
+                        Image("Image 2")
+                            .resizable()
+                            .background(Image("Laranja"))
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(100)
+                            .padding(8)
+                        Text("Ruan da hora")
+                            .font(.title2.bold())
+                            .foregroundColor(.primary)
+                        
+                        Spacer()
+                    }
+                }
+                Spacer()
+                HStack{
+                    
+                    TextField("  Digite", text: self.$search)
+                        .frame(width: 250, height: 40)
+                        .foregroundColor(.primary)
+                        .background(.ultraThinMaterial)
+                        .cornerRadius(20)
+                    
+                }
+            }
+        }
     }
 }
