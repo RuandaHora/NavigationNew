895e720da20d29cd40ff8698135b969d09ea6516
Ruan da Hora <ruanpablosnake@gmail.com>
2023-01-22 17:06:13 -0300

    LoadingView Update

--- View/LoadingView.swift
+++ View/LoadingView.swift
@@ -1,8 +1,16 @@
 import SwiftUI
 
 struct LoadingView: View {
+    
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    
     var body: some View {
-        ZStack {
+        if isActive {
+            GeneralView()
+        }
+        else {
             VStack{
                 Image("Logo")
                     .resizable()
@@ -10,6 +18,21 @@ struct LoadingView: View {
                     .cornerRadius(35)
                 Text("Navigation")
                     .font(.title2.bold())
+            }
+            .opacity(size)
+            .opacity(opacity)
+            .onAppear{
+                withAnimation(.easeIn(duration: 1.2)){
+                    self.size = 0.9
+                    self.opacity = 1.0
+                }
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+                    withAnimation{
+                        self.isActive = true
+                    }
+                }
             }
         }
     }
